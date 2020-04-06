ac91c54d9c301a86504247c11d90f0d1d8e5d4b9
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2020-04-07 00:17:22 +0200

    angular certificates list

--- defence-first-web/src/app/pki/certificates/certificates.component.ts
+++ defence-first-web/src/app/pki/certificates/certificates.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PkiServiceService } from 'src/app/services/pki-service.service';
+import { SimpleCertificate } from 'src/app/models/simple-certificate.model';
 
 @Component({
   selector: 'app-certificates',
@@ -8,16 +9,18 @@ import { PkiServiceService } from 'src/app/services/pki-service.service';
 })
 export class CertificatesComponent implements OnInit {
 
+  certificates: SimpleCertificate[];
+
   constructor(
     private pkiService: PkiServiceService
   ) { }
 
   ngOnInit() {
     this.pkiService.getCertificates().subscribe(
-      data => {
-        console.log(data);
+      (data: SimpleCertificate[]) => {
+        this.certificates = data;
       }
-    )
+    );
   }
 
 }
