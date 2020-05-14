061b4132f69125ee460917e3f1f8d9da48f015f0
DusanBucan <dusanbzr@gmail.com>
2020-05-14 18:33:46 +0200

    aa

--- siem-agent/src/main/java/rs/ac/uns/ftn/siemagent/config/MyTrustStrategy.java
+++ siem-agent/src/main/java/rs/ac/uns/ftn/siemagent/config/MyTrustStrategy.java
@@ -1,19 +1,45 @@
 package rs.ac.uns.ftn.siemagent.config;
 
 import org.apache.http.ssl.TrustStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.*;
+import java.util.Arrays;
 
 public class MyTrustStrategy implements TrustStrategy {
 
 
-    
+    // sto je ovo Null...
+    @Autowired @Qualifier("myKeyStore")
+    private KeyStore trustStore;
+
     @Override
-    public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        for(X509Certificate certificate : chain) {
-            System.out.println(certificate.getSerialNumber());
+    public boolean isTrusted(X509Certificate[] chain, String authType) {
+        boolean retVal = true;
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            CertPath path = cf.generateCertPath(Arrays.asList(chain));
+            CertPathValidator validator = CertPathValidator.getInstance("PKIX");
+            PKIXParameters params = new PKIXParameters(trustStore);
+            PKIXCertPathValidatorResult r = (PKIXCertPathValidatorResult) validator.validate(path, params);
         }
-        return true;
+        catch (NoSuchAlgorithmException | CertificateException | KeyStoreException | InvalidAlgorithmParameterException e){e.printStackTrace();}
+        catch (CertPathValidatorException e){
+            e.printStackTrace();
+            retVal = false;
+        }finally {
+            return retVal;
+        }
+
+//        for(X509Certificate certificate : chain) {
+//            System.out.println(certificate.getSerialNumber());
+//        }
+//        return true;
     }
 }
