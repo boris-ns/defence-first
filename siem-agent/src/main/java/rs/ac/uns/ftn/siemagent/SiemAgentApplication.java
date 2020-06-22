b06282aba49ddd99c5aa51d853e17ea1cfef5f3e
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2020-06-22 20:08:29 +0200

    windows logs

--- siem-agent/src/main/java/rs/ac/uns/ftn/siemagent/SiemAgentApplication.java
+++ siem-agent/src/main/java/rs/ac/uns/ftn/siemagent/SiemAgentApplication.java
@@ -1,27 +1,14 @@
 package rs.ac.uns.ftn.siemagent;
 
-import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import rs.ac.uns.ftn.siemagent.Constants.Constants;
-import rs.ac.uns.ftn.siemagent.model.Log;
-import rs.ac.uns.ftn.siemagent.model.LogType;
 import rs.ac.uns.ftn.siemagent.repository.Keystore;
 import rs.ac.uns.ftn.siemagent.service.CertificateService;
 import rs.ac.uns.ftn.siemagent.service.LogReader;
 import rs.ac.uns.ftn.siemagent.service.LogService;
 import rs.ac.uns.ftn.siemagent.service.OCSPService;
-
-import javax.crypto.SecretKey;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.StringWriter;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.util.ArrayList;
-import java.util.Date;
 
 @SpringBootApplication
 public class SiemAgentApplication implements CommandLineRunner {
@@ -69,19 +56,5 @@ public class SiemAgentApplication implements CommandLineRunner {
 //		certificateService.sendReplaceCertificateRequest(token);
 	}
 
-	// TODO: obrisati
-	private void simulation() throws Exception{
-
-//		ArrayList<Log> logs = new ArrayList<Log>();
-//		logs.add(new Log(1l, new Date(), LogType.SUCCESS, "prviLog", "ja"));
-//		logs.add(new Log(2l, new Date(), LogType.SUCCESS, "drugiLog", "ja"));
-//		logService.sendLogs(logs);
-//
-//		logs.clear();
-//		logs.add(new Log(3l, new Date(), LogType.ERROR, "treciLog", "ja"));
-//		logs.add(new Log(4l, new Date(), LogType.WARN, "certvrtiLog", "ja"));
-
-//		logService.sendLogs(logs);
-	}
 
 }
