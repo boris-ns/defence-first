c35397c8ed658c3e62c2b984da6625ea9989c4ac
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2020-06-25 17:25:57 +0200

    finish severity

--- defence-first-web/src/app/models/log.model.ts
+++ defence-first-web/src/app/models/log.model.ts
@@ -2,12 +2,12 @@ export interface Log {
     id: number;
     date: Date;
     logType: LogType;
-    sevetity: number;
+    severity: number;
     message: string;
     source: string;
     agent: string;
 }
 
 export enum LogType {
-    INFO, WARN, ERROR, SUCCESS
+    INFO, WARN, ERROR, SUCCESS, DEBUG, OTHER, SUCCESSFUL_LOGIN, UNSUCCESSFUL_LOGIN
 }
